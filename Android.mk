f8dcc23d2aaed5221bf1035a70c7ba1cdc91908e
Ricardo Cerqueira <github@cerqueira.org>
2011-07-04 03:15:31 +0100

    CMScreenshot: Build the native part by default
    This is useless without the helper, so build it always

--- Android.mk
+++ Android.mk
@@ -1,8 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CMScreenshot
@@ -10,5 +8,22 @@ LOCAL_CERTIFICATE := platform
 
 LOCAL_SDK_VERSION := current
 
+LOCAL_MODULE_TAGS := optional
+
 include $(BUILD_PACKAGE)
 
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := \
+        libcutils \
+        libutils \
+        libbinder \
+        libui \
+        libsurfaceflinger_client
+
+LOCAL_SRC_FILES:= native/ss.cpp
+
+LOCAL_MODULE:= screenshot
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
