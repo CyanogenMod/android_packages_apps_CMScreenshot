70e4b0b001e1aa90535e9dcb8ad96cb68a3cdfc9
Danny Baumann <dannybaumann@web.de>
2012-09-21 12:54:56 +0200

    Overhaul CMScreenshot.
    - Use a meaningful file name format
    - Don't keep bitmaps around
    - Fix coding style
    - Don't call scanFile() on media scanner before service is connected,
      otherwise this will happen:
    
    E/AndroidRuntime(23921): FATAL EXCEPTION: main
    E/AndroidRuntime(23921): java.lang.IllegalStateException: not connected to MediaScannerService
    E/AndroidRuntime(23921): 	at android.media.MediaScannerConnection.scanFile(MediaScannerConnection.java:164)
    E/AndroidRuntime(23921): 	at com.cyanogenmod.screenshot.ScreenshotActivity.takeScreenshot(ScreenshotActivity.java:154)
    E/AndroidRuntime(23921): 	at com.cyanogenmod.screenshot.ScreenshotActivity$2.run(ScreenshotActivity.java:175)
    E/AndroidRuntime(23921): 	at android.os.Handler.handleCallback(Handler.java:587)
    E/AndroidRuntime(23921): 	at android.os.Handler.dispatchMessage(Handler.java:92)
    E/AndroidRuntime(23921): 	at android.os.Looper.loop(Looper.java:130)
    E/AndroidRuntime(23921): 	at android.app.ActivityThread.main(ActivityThread.java:3835)
    E/AndroidRuntime(23921): 	at java.lang.reflect.Method.invokeNative(Native Method)
    E/AndroidRuntime(23921): 	at java.lang.reflect.Method.invoke(Method.java:507)
    E/AndroidRuntime(23921): 	at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:864)
    E/AndroidRuntime(23921): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:622)
    E/AndroidRuntime(23921): 	at dalvik.system.NativeStart.main(Native Method)
    W/ActivityManager(2290):   Force finishing activity com.cyanogenmod.screenshot/.ScreenshotActivity
    
    Change-Id: I3149e6046a34a080e1018c3381455c6d15ba42db

--- src/com/cyanogenmod/screenshot/ScreenshotActivity.java
+++ src/com/cyanogenmod/screenshot/ScreenshotActivity.java
@@ -1,18 +1,18 @@
 /*
 **
 ** Copyright 2010, Koushik Dutta
-** Copyright 2011, The CyanogenMod Project 
+** Copyright 2011, The CyanogenMod Project
 **
-** Licensed under the Apache License, Version 2.0 (the "License"); 
-** you may not use this file except in compliance with the License. 
-** You may obtain a copy of the License at 
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
 **
-**       http://www.apache.org/licenses/LICENSE-2.0 
+**       http://www.apache.org/licenses/LICENSE-2.0
 **
-** Unless required by applicable law or agreed to in writing, software 
-** distributed under the License is distributed on an "AS IS" BASIS, 
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-** See the License for the specific language governing permissions and 
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
 ** limitations under the License.
 */
 
@@ -21,6 +21,7 @@ package com.cyanogenmod.screenshot;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -39,142 +40,131 @@ import android.os.Handler;
 import android.os.Environment;
 import android.os.SystemProperties;
 import android.provider.Settings;
+import android.text.format.DateFormat;
 import android.util.Log;
-import android.widget.Toast;
-
 import android.view.Display;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class ScreenshotActivity extends Activity
 {
-    static Bitmap mBitmap = null;
-    Handler mHander = new Handler();
-    String mScreenshotFile;
+    private static final String TAG = "CMScreenshot";
 
     private static final String SCREENSHOT_BUCKET_NAME =
         Environment.getExternalStorageDirectory().toString()
         + "/DCIM/Screenshots";
 
+    private static final String DATEFORMAT_12 = "yyyyMMdd-hhmmssaa";
+    private static final String DATEFORMAT_24 = "yyyyMMdd-kkmmss";
+
+    Handler mHander = new Handler();
+    String mScreenshotFile;
+    MediaScannerConnection mConnection;
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (!(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))) {
             Toast toast = Toast.makeText(ScreenshotActivity.this, getString(R.string.not_mounted), Toast.LENGTH_LONG);
             toast.show();
             finish();
         }
+
         mConnection = new MediaScannerConnection(ScreenshotActivity.this, mMediaScannerConnectionClient);
-        mConnection.connect();
         takeScreenshot(1);
     }
 
-    void takeScreenshot()
-    {
-        String mRawScreenshot = String.format("%s/tmpshot.bmp", Environment.getExternalStorageDirectory().toString());
+    void takeScreenshot() {
+        String rawScreenshot = String.format("%s/tmpshot.bmp", Environment.getExternalStorageDirectory().toString());
 
-        try
-        {
+        try {
             Process p = Runtime.getRuntime().exec("/system/bin/screenshot");
-            Log.d("CMScreenshot","Ran helper");
+            Log.d(TAG, "Ran helper");
             p.waitFor();
-            mBitmap = BitmapFactory.decodeFile(mRawScreenshot);
-            File tmpshot = new File(mRawScreenshot);
+
+            Bitmap bitmap = BitmapFactory.decodeFile(rawScreenshot);
+            File tmpshot = new File(rawScreenshot);
             tmpshot.delete();
 
-            if (mBitmap == null) {
-                throw new Exception("Unable to save screenshot: mBitmap = "+mBitmap);
+            if (bitmap == null) {
+                throw new Exception("Unable to save screenshot");
             }
 
             // valid values for ro.sf.hwrotation are 0, 90, 180 & 270
-            int rot = 360-SystemProperties.getInt("ro.sf.hwrotation",0);
+            int hwRotation = (360 - SystemProperties.getInt("ro.sf.hwrotation",0)) % 360;
 
             Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+            int deviceRotation = (display.getOrientation() * 90) % 360;
 
-            // First round, natural device rotation
-            if(rot > 0 && rot < 360){
-                Log.d("CMScreenshot","rotation="+rot);
+            int finalRotation = hwRotation - deviceRotation;
+            Log.d(TAG, "Hardware rotation " + hwRotation + ", device rotation " +
+                    deviceRotation + " -> rotate by " + finalRotation);
+
+            if (finalRotation != 0) {
                 Matrix matrix = new Matrix();
-                matrix.postRotate(rot);
-                mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
+                matrix.postRotate(finalRotation);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
             }
 
-            // Second round, device orientation:
-            // getOrientation returns 0-3 for 0, 90, 180, 270, relative
-            // to the natural position of the device
-            rot = (display.getOrientation() * 90);
-            rot %= 360;
-            if(rot > 0){
-                Log.d("CMScreenshot","rotation="+rot);
-                Matrix matrix = new Matrix();
-                matrix.postRotate((rot*-1));
-                mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
-            }
+            File dir = new File(SCREENSHOT_BUCKET_NAME);
+            if (!dir.exists()) dir.mkdirs();
 
-            try
-            {
-                File dir = new File(SCREENSHOT_BUCKET_NAME);
-                if (!dir.exists()) dir.mkdirs();
-                mScreenshotFile = String.format("%s/screenshot-%d.png", SCREENSHOT_BUCKET_NAME, System.currentTimeMillis());
-                FileOutputStream fout = new FileOutputStream(mScreenshotFile);
-                mBitmap.compress(CompressFormat.PNG, 100, fout);
-                fout.close();
+            CharSequence date = DateFormat.format(
+                    DateFormat.is24HourFormat(this) ? DATEFORMAT_24 : DATEFORMAT_12, new Date());
 
-                boolean shareScreenshot = (Settings.System.getInt(getContentResolver(),
-                                               Settings.System.SHARE_SCREENSHOT, 0)) == 1;
-                if (shareScreenshot) {
+            mScreenshotFile = String.format("%s/screenshot-%s.png", SCREENSHOT_BUCKET_NAME, date);
+            FileOutputStream fout = new FileOutputStream(mScreenshotFile);
+            bitmap.compress(CompressFormat.PNG, 100, fout);
+            fout.close();
+
+            boolean shareScreenshot = (Settings.System.getInt(getContentResolver(),
+                    Settings.System.SHARE_SCREENSHOT, 0)) == 1;
+
+            if (shareScreenshot) {
+                try {
                     Intent intent = new Intent(Intent.ACTION_SEND);
                     intent.setType("image/png");
 
                     intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(mScreenshotFile)));
 
                     startActivity(Intent.createChooser(intent, getString(R.string.share_message)));
+                } catch (android.content.ActivityNotFoundException e) {
+                    Toast.makeText(ScreenshotActivity.this, R.string.no_way_to_share, Toast.LENGTH_SHORT).show();
                 }
             }
-            catch (android.content.ActivityNotFoundException ex) {
-                Toast.makeText(ScreenshotActivity.this,
-                        R.string.no_way_to_share,
-                        Toast.LENGTH_SHORT).show();
-            }
-            catch (Exception ex)
-            {
-                finish();
-                throw new Exception("Unable to save screenshot: "+ex);
-            }
-        }
-        catch (Exception ex)
-        {
-            Toast toast = Toast.makeText(ScreenshotActivity.this, getString(R.string.toast_error) + " " + ex.getMessage(), Toast.LENGTH_LONG);
+        } catch (Exception e) {
+            Log.e(TAG, "Couldn't save screenshot", e);
+            Toast toast = Toast.makeText(ScreenshotActivity.this, getString(R.string.toast_error), Toast.LENGTH_LONG);
             toast.show();
+            finish();
         }
+
         Toast toast = Toast.makeText(ScreenshotActivity.this, getString(R.string.toast_save_location) + " " + mScreenshotFile,Toast.LENGTH_LONG);
         toast.show();
 
-        mConnection.scanFile(mScreenshotFile, null);
-        mConnection.disconnect();
-        finish();
+        mConnection.connect();
     }
 
-    MediaScannerConnection mConnection;
     MediaScannerConnection.MediaScannerConnectionClient mMediaScannerConnectionClient = new MediaScannerConnection.MediaScannerConnectionClient() {
         public void onScanCompleted(String path, Uri uri) {
+            Log.d(TAG, "Scan of " + path + " completed -> uri " + uri);
             mConnection.disconnect();
             finish();
         }
         public void onMediaScannerConnected() {
+            Log.d(TAG, "Connected to media scanner, scanning " + mScreenshotFile);
+            mConnection.scanFile(mScreenshotFile, null);
         }
     };
 
-    void takeScreenshot(final int delay)
-    {
-        mHander.postDelayed(new Runnable()
-        {
-            public void run()
-            {
+    void takeScreenshot(final int delay) {
+        mHander.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 takeScreenshot();
             }
         }, delay * 1000);
     }
-
 }
